be3bad0b44739afe44635bc5cf57b18cb7f6d9ba
alejandro <levinasale@gmail.com>
2017-10-19 18:44:58 -0300

    Agrego chequeo de ZeroDivisionError y docu

--- fraccion.py
+++ fraccion.py
@@ -1,9 +1,14 @@
 class Fraccion:
+    '''
+    La clase representa a una Fraccion con un numerador y un denominador
+    '''
 	def __init__(self, numerador, denominador):
-		self.numerador = numerador
-		self.denominador = denominador
+		self.num = numerador
+		self.den = denominador
+        if denominador == 0:
+            raise ZeroDivisionError("No se puede dividir por 0")
 
 	def __mul__(self, otra):
-		nuevo_numerador = self.numerador*otra.numerador
-		nuevo_denominador = self.denominador*otra.denominador
+		nuevo_numerador = self.num*otra.num
+		nuevo_denominador = self.den*otra.den
 		return Fraccion(nuevo_numerador, nuevo_denominador)
